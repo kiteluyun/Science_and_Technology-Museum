4a885c5774cb9ac922dc28612600c319281d2c8b
kiteluyun <2216828801@qq.com>
2024-06-27 18:59:07 +0800

    Your commit message

---
+++ 科学展示/js/index1.js
@@ -0,0 +1,8 @@
+jQuery(".picMarquee-left").slide({
+		        mainCell: ".bd",
+		        autoPlay: true,
+		        effect: "leftMarquee",
+		        vis: 3,
+		        interTime: 0,
+		        trigger: "click"
+		    });
